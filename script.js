8b617d31c5adb6e45bed30a15161eaf769725505
Carlos Adames <adames@ouroanalytics.ai>
2024-02-04 22:02:14 -0400

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Select all cells
-var cells = document.querySelectorAll('.cell');
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Select all cells
+    var cells = document.querySelectorAll('.cell');
 
-// Loop through each cell
-for (var i = 0; i < cells.length; i++) {
-    // Add click event listener to each cell
-    cells[i].addEventListener('click', function() {
-        // Do something when cell is clicked
-        alert('Cell clicked!');
-    });
-}
+    // Loop through each cell
+    for (var i = 0; i < cells.length; i++) {
+        // Add click event listener to each cell
+        cells[i].addEventListener('click', function() {
+            // Add 'X' to the clicked cell
+            this.innerHTML = 'X';
+        });
+    }
+});
