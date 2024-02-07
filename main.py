0771672f94ae0e918efc5ef36d6da663f3d756ec
Carlos Adames <adames@ouroanalytics.ai>
2024-02-07 00:20:53 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -48,21 +48,31 @@ def grid_map(cell_id):
 def home():
     return render_template('index.html')
 
-@app.route('/usr', methods=['POST'])
-def player_mark():
-    # Get JSON data from request to choose mark
-    data = request.get_json()
-    mark = data.get('mark')
-    slot = data.get('cell')
-    row, col = board_map(slot)
-    game.mark(row, col)
-    return jsonify({'mark': mark})
-
 @app.route('/reset', methods=['POST'])
 def reset_game():
     # Reset the game state
     game.reset()
     return jsonify({'status': 'Game reset'})
+
+#@app.route('/usr', methods=['POST'])
+#def player_mark():
+    # Get JSON data from request to choose mark
+#    data = request.get_json()
+#    mark = data.get('mark')
+#    slot = data.get('cell')
+#    row, col = board_map(slot)
+#    game.mark(row, col)
+#    return jsonify({'mark': mark})
+
+#@app.route('/usrmove', methods=['POST'])
+#def player_move():
+    # Get JSON data from request to choose mark
+#    data = request.get_json()
+#    mark = data.get('mark')
+#    slot = data.get('cell')
+#    row, col = board_map(slot)
+#    game.mark(row, col)
+#    return jsonify({'mark': mark})
 
 #@app.route('/pcmove', methods=['GET'])
 #def pc_move():
@@ -71,15 +81,7 @@ def reset_game():
 #    pc_mark = game.player
 #    return jsonify({'pc_mark':pc_mark, 'pc_move':pc_move})
 
-@app.route('/usrmove', methods=['POST'])
-def player_move():
-    # Get JSON data from request to choose mark
-    data = request.get_json()
-    mark = data.get('mark')
-    slot = data.get('cell')
-    row, col = board_map(slot)
-    game.mark(row, col)
-    return jsonify({'mark': mark})
+
 
 if __name__ == '__main__':
     app.run(port=8000)
